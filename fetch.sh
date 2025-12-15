91c3ce090c46d2387f47d3fd021133dc4956d9a0
Mingyang <kohmingyang@outlook.com>
2025-12-15 18:24:16 +0800

    feat: improved fetch script and template

--- fetch.sh
+++ fetch.sh
@@ -32,12 +32,12 @@ fi
 # Generate template and open with VS code
 if [ ! -f "challenge_1.cpp" ];
 then
-    cat ../template.cpp > challenge_1.cpp
+    sed "s/day_X/day_$day/g" ../template.cpp > challenge_1.cpp
 fi
 
 if [ ! -f "challenge_2.cpp" ];
 then
-    cat ../template.cpp > challenge_2.cpp
+    sed "s/day_X/day_$day/g" ../template.cpp > challenge_2.cpp
 fi
 code .. # open parent directory
 
--- template.cpp
+++ template.cpp
@@ -1,20 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 using namespace std;
 
+class Solution
+{
+private:
+public:
+    int solve(ifstream &file)
+    {
+        string line;
+        getline(file, line);
+
+        return 0;
+    }
+
+    void test()
+    {
+        ifstream testfile("day_X/mock.txt");
+        const int ANSWER = 0;
+
+        assert(solve(testfile) == ANSWER && "Test case not passed");
+
+        testfile.close();
+    }
+
+    int run()
+    {
+        ifstream infile("day_X/input.txt");
+
+        int ans = solve(infile);
+
+        cout << "The answer is: " << ans << endl;
+
+        infile.close();
+
+        return 0;
+    }
+};
+
 int main()
 {
-    ifstream infile("input.txt");
-
-    string line;
-    getline(infile, line);
-
-    int ans = 0;
-
-    cout << "The answer is: " << ans << endl;
-
-    infile.close();
-
-    return 0;
+    Solution challenge;
+    challenge.test();
+    return challenge.run();
 }
